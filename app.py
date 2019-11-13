ef450c731cf5b169e715fe505a32df0142d3f9e9
Richarlotte <charlotteadolfssonolander@gmail.com>
2019-11-13 11:48:27 +0100

    trying with adding a file to requriments

--- app.py
+++ app.py
@@ -14,12 +14,14 @@ def about():
     data = []
     with open("data/company.json", "r") as json_data:
         data = json.load(json_data)
-    return render_template("about.html", page_title="About", 
-    company = data) 
-    #page_title is a serverside set variable
+    return render_template("about.html", page_title="About",
+                           company=data)
+    # page_title is a serverside set variable
+
     """
     list_of_numbers is a list created and added in base to not repeat ourselfs
     """
+
 
 @app.route("/about/<member_name>")
 def about_member(member_name):
@@ -31,12 +33,12 @@ def about_member(member_name):
             if obj["url"] == member_name:
                 member = obj
 
-    return render_template("member.html", member=member )
+    return render_template("member.html", member=member)
+
 
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
     if request.method == "POST":
-        flash("Thanks {}, we have recieved your message!".format(request.form["name"]))
+        flash("Thanks {}, we have recieved your message!".format(
+            request.form["name"]))
     return render_template("contact.html", page_title="Contact")
-
-
